f29f3cdcb0dcb4e1ae9b1cb093ddca68a2bb861e
Somesh Prajapati <someshprajapati@gmail.com>
2021-08-17 12:16:37 +0530

    Initial commit with variable experiment

---
+++ terraform-test/resource.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  
+}
+
+variable "AWS_REGION" {
+    type = string
+}
+resource "aws_instance" "example" {
+    ami = "${var.AMIS[var.AWS_REGION]}"
+    instance_type = "t2.micro"
+}
