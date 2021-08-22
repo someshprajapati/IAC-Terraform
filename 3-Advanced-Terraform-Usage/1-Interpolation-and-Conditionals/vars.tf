e1a23bce192beb5019920d362dc20652ed116eb3
Somesh Prajapati <someshprajapati@gmail.com>
2021-08-22 16:43:15 +0530

    Interpolation and Conditionals in terraform

---
+++ 3-Advanced-Terraform-Usage/1-Interpolation-and-Conditionals/vars.tf
@@ -0,0 +1,16 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/id_rsa_personal"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/id_rsa_personal.pub"
+}
+
+variable "ENV" {
+  default = "prod"
+}
+
