c1bbe50a08b7bfe59c61b5ae37c2998d2e70ebe9
Somesh Prajapati <someshprajapati@gmail.com>
2021-08-22 20:45:12 +0530

    For and For-each loop in terraform

---
+++ 3-Advanced-Terraform-Usage/3-For-and-For-Each-Loops/for-demo/vars.tf
@@ -0,0 +1,34 @@
+variable "list1" {
+  type = list(string)
+  default = [1, 10, 9, 101, 3]
+}
+
+variable "list2" {
+  type = list(string)
+  default = ["apple", "pear", "banana", "mango"]
+}
+
+variable "map1" {
+  type = map(number)
+  default = {
+   "apple" = 5
+   "pear" = 3
+   "banana" = 10
+   "mango" = 0
+  }
+}
+
+variable "AWS_REGION" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "project_tags" {
+  type          = map(string)
+  default       = {
+    Component   = "Frontend"
+    Environment = "Production"
+  }
+}
+
+
