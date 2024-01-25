f7df2cd7fc91839ce75901cf99e1eb7cb3c38f1e
soprajap <someshp@juniper.net>
2024-01-25 23:24:49 +0530

    Added the code for packer

---
+++ 4-Packer/amivar.tf
@@ -0,0 +1,1 @@
+variable "AMI_ID" { default = "ami-0d55481a757f18571" }
---
+++ 4-Packer/vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/id_rsa_personal"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/id_rsa_personal.pub"
+}
