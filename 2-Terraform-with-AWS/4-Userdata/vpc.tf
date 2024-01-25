fabe97574e7cc7fc74a09347b05a8a8002b347a2
soprajap <someshp@juniper.net>
2024-01-25 14:26:11 +0530

    Fix the errors and added the graph

--- 2-Terraform-with-AWS/4-Userdata/vpc.tf
+++ 2-Terraform-with-AWS/4-Userdata/vpc.tf
@@ -4,7 +4,6 @@ resource "aws_vpc" "main" {
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
   tags = {
     Name = "main"
   }
@@ -15,7 +14,7 @@ resource "aws_subnet" "main-public-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1a"
+  availability_zone       = "ap-south-1a"
 
   tags = {
     Name = "main-public-1"
@@ -26,7 +25,7 @@ resource "aws_subnet" "main-public-2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1b"
+  availability_zone       = "ap-south-1b"
 
   tags = {
     Name = "main-public-2"
@@ -37,7 +36,7 @@ resource "aws_subnet" "main-public-3" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1c"
+  availability_zone       = "ap-south-1c"
 
   tags = {
     Name = "main-public-3"
@@ -48,7 +47,7 @@ resource "aws_subnet" "main-private-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1a"
+  availability_zone       = "ap-south-1a"
 
   tags = {
     Name = "main-private-1"
@@ -59,7 +58,7 @@ resource "aws_subnet" "main-private-2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1b"
+  availability_zone       = "ap-south-1b"
 
   tags = {
     Name = "main-private-2"
@@ -70,7 +69,7 @@ resource "aws_subnet" "main-private-3" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1c"
+  availability_zone       = "ap-south-1c"
 
   tags = {
     Name = "main-private-3"
