1a7d353e18b554e876f0eb102d9a5a9f0f559009
soprajap <someshp@juniper.net>
2024-01-25 19:02:18 +0530

    Added the code for Elastic beanstalk

---
+++ 2-Terraform-with-AWS/11-Elastic-Beanstalk/vars.tf
@@ -0,0 +1,14 @@
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/id_rsa_personal"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/id_rsa_personal.pub"
+}
+
+variable "RDS_PASSWORD" {
+}
